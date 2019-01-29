2cb7736494cb18c0e0a2e13fca1f7d4f9fc1fe8e
Hannah campbell <hannahcampbell@Hannahs-MacBook-Pro.local>
2019-01-28 21:44:31 -0500

    first commit

---
+++ Quizzler/Quizzler-Bridging-Header.h
@@ -0,0 +1,21 @@
+//
+//  Quizzler-Bridging-Header.h
+//  Quizzler
+//
+//  Created by Hannah campbell on 1/28/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+#ifndef Quizzler_Bridging_Header_h
+#define Quizzler_Bridging_Header_h
+
+
+#endif /* Quizzler_Bridging_Header_h */
+
+#ifndef Quizzler_Bridging_Header_h
+
+#define Quizzler_Bridging_Header_h
+
+#endif /* Quizzler_Bridging_Header_h */
+
+#import "ProgressHUD.h"
